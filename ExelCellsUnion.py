b19a29216ff247e471698808acdad69a79a906ab
William Grimaldi <w.grimaldi@proton.me>
2025-09-01 21:53:30 +0200

    Update

--- ExelCellsUnion.py
+++ ExelCellsUnion.py
@@ -1,4 +1,7 @@
 import os
+import threading
+
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
@@ -15,27 +18,34 @@ file_path2 = "C:\\Users\\AdminAkro\\Downloads\\[WMATA] Trigger Condition Faults 
 s_name2 = 0 #nome foglio da confrontare
 col_name2 ='TriggerCondition' #nome colonna da confrontare
 file_path_output = "C:\\Users\\AdminAkro\\Downloads\\out.xlsx"
-col1 = ""
-col2 = ""
+col1 = []
+col2 = []
 col3 = []
+cell3 = ""
 threshold = 0.3
 
 def are_conceptually_similar(sentence1, sentence2):
- embedding1 = model.encode(sentence1, convert_to_tensor=True)
- embedding2 = model.encode(sentence2, convert_to_tensor=True)
- cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2).item()
- return cosine_similarity
+    if sentence1 == sentence2:
+        return True  # Salta il confronto
+    else:
+        embedding1 = model.encode(sentence1, convert_to_tensor=True)
+        embedding2 = model.encode(sentence2, convert_to_tensor=True)
+        cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2).item()
+        return cosine_similarity
 
 def read_excel_column(file_path, s_name, col_name):
     xls = pd.read_excel(file_path, s_name)
-    colonna = xls[col_name]
-    col = colonna.astype(str).tolist()
+    column = xls[col_name]
+    col = column.astype(str).tolist()
     return col
 
 def read_excel_row(file_path, s_name, row_index):
     xls = pd.read_excel(file_path, s_name)
-    riga = xls[row_index].tolist()
-    return riga
+    #print(row_index)
+    riga = xls.iloc[row_index]
+    r = riga.astype(str).tolist()
+    #print(r)
+    return r
 
 def list_of_sheets(file_path):
     xls = pd.ExcelFile(file_path, engine="openpyxl")
@@ -50,46 +60,76 @@ def write_excel_with_dataframe(file_path, df):
         else:
             with pd.ExcelWriter(file_path, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
                 df.to_excel(writer, sheet_name=sheet1)
-        print(f"Scritto il file {file_path}")
+        print(f"Scritto il file {file_path} in {sheet1}")
     except Exception as e:
         print(f"Errore durante la scrittura del file {file_path}")
 
 def cell_union(cell1, cell2):
     if are_conceptually_similar(cell1, cell2) < threshold:
-        return f"{cell1} \r\n\r\n {cell2}"
+        return f"{cell1} \r\n pippo \r\n {cell2}"
     else:
         return f"{cell1}"
 
-def row_extract(sheet1):
+def number_of_row(file_path, sheet):
+    try:
+        xls = pd.read_excel(file_path, sheet)
+        return xls.shape[0]  # Restituisce il numero di righe
+    except Exception as e:
+        print(f"Errore durante il conteggio delle righe nel foglio {sheet}: {e}")
+        return -1  # Indicatore di errore
 
+def get_cell(row_index, col_name, sheet, file_path):
+    try:
+        # Leggi il file Excel
+        xls = pd.read_excel(file_path, sheet_name=sheet)
+        # Recupera il valore della cella
+        cell_value = xls.at[row_index, col_name]
+        return str(cell_value)  # Restituisce il valore della cella come stringa
+    except Exception as e:
+        print(f"Errore durante l'accesso alla cella nella riga {row_index}, colonna {col_name} nel foglio {sheet}: {e}")
+        return None
+
+def cut_row(row, index_row_cut=9):
+    return row[:index_row_cut]
+
+def process_sheets(sheet1, sheet2):
+    already_compared = set()
+    try:
+        col1 = read_excel_column(file_path1, sheet1, col_name1)
+        col2 = read_excel_column(file_path2, sheet2, col_name2)
+        for row_index1 in range(1, row_number1 + 1):
+            for row_index2 in range(1, row_number2 + 1):
+                row1 = cut_row(read_excel_row(file_path1, sheet1, row_index=row_index1))
+                row2 = cut_row(read_excel_row(file_path1, sheet1, row_index=row_index2))
+                if row1 == row2:
+                    cell1 = get_cell(row_index1, col_name1, sheet1, file_path1)
+                    cell2 = get_cell(row_index2, col_name2, sheet2, file_path2)
+                    if (cell1, cell2) not in already_compared or (cell2, cell1) not in already_compared:
+                        cell3 = cell_union(cell1, cell2)
+                        already_compared.add((cell1, cell2))
+                        print(cell3)
+                        col3.append(cell3)
+                    write_excel_with_dataframe(file_path_output, pd.DataFrame({"TriggerCondition" :col3}))
+    except Exception as e:
+        print(f"La pagina {sheet2} nel file {file_path2} non ha una colonna chiamata {col_name2}")
 
 if __name__ == '__main__':
     sheet_names1 = list_of_sheets(file_path1)
     sheet_names2 = list_of_sheets(file_path2)
-    for sheet1 in sheet_names1:
-        try:
-            col1 = read_excel_column(file_path1, sheet1, col_name1)
+    threads = []
+    with ThreadPoolExecutor() as executor:
+
+        for sheet1 in sheet_names1:
+            row_number1 = number_of_row(file_path1, sheet1)
             for sheet2 in sheet_names2:
-                try:
-                    col2 = read_excel_column(file_path2, sheet2, col_name2)
-                    print("Inizio il confronto tra le celle")
-                    riga1 = read_excel_row(file_path1, sheet1, row_index)
-                    if sheet1 == sheet2 :
-                        cell3 = """Hello World!"""
-                        col3 = []
+                row_number2 = number_of_row(file_path2, sheet2)
+                if sheet1 == sheet2:
+                    print(f"Analizzo il foglio {sheet1}")
+                    executor.submit(process_sheets, sheet1, sheet2)
+                    #thread = threading.Thread(target=process_sheets, args=(sheet1, sheet2))
+                    #threads.append(thread)
+                    #thread.start()
+        for thread in threads:
+            thread.join()
 
-                        row_cuted = df.iloc[riga1, :9].tolist() # Prima riga, prime 3 colonne
-
-                        if riga1 == riga2:
-
-                            for i, (cell1, cell2) in enumerate(zip(col1, col2)):
-                                col3 = cell_union(cell1, cell2)
-                        else:
-                            cella1
-                        col3.append(cell3)
-                        write_excel_with_dataframe(file_path_output, pd.DataFrame({"TriggerCondition" :col3}))
-                except Exception as e:
-                    print(f"La pagina {sheet2} nel file {file_path2} non ha una colonna chiamata {col_name2}")
-        except Exception as e:
-            print(f"La pagina {sheet1} nel file {file_path1} non ha una colonna chiamata {col_name1}")
-    print("Ho finito")
+print("Ho finito")
--- tmp.py
+++ tmp.py
@@ -1,4 +1,7 @@
 import os
+import threading
+
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
@@ -15,16 +18,20 @@ file_path2 = "C:\\Users\\AdminAkro\\Downloads\\[WMATA] Trigger Condition Faults 
 s_name2 = 0 #nome foglio da confrontare
 col_name2 ='TriggerCondition' #nome colonna da confrontare
 file_path_output = "C:\\Users\\AdminAkro\\Downloads\\out.xlsx"
-col1 = ""
-col2 = ""
+col1 = []
+col2 = []
 col3 = []
+cell3 = ""
 threshold = 0.3
 
 def are_conceptually_similar(sentence1, sentence2):
-    embedding1 = model.encode(sentence1, convert_to_tensor=True)
-    embedding2 = model.encode(sentence2, convert_to_tensor=True)
-    cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2).item()
-    return cosine_similarity
+    if sentence1 == sentence2:
+        return True  # Salta il confronto
+    else:
+        embedding1 = model.encode(sentence1, convert_to_tensor=True)
+        embedding2 = model.encode(sentence2, convert_to_tensor=True)
+        cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2).item()
+        return cosine_similarity
 
 def read_excel_column(file_path, s_name, col_name):
     xls = pd.read_excel(file_path, s_name)
@@ -34,10 +41,10 @@ def read_excel_column(file_path, s_name, col_name):
 
 def read_excel_row(file_path, s_name, row_index):
     xls = pd.read_excel(file_path, s_name)
-    print(row_index)
+    #print(row_index)
     riga = xls.iloc[row_index]
     r = riga.astype(str).tolist()
-    print(r)
+    #print(r)
     return r
 
 def list_of_sheets(file_path):
@@ -53,13 +60,13 @@ def write_excel_with_dataframe(file_path, df):
         else:
             with pd.ExcelWriter(file_path, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
                 df.to_excel(writer, sheet_name=sheet1)
-        print(f"Scritto il file {file_path}")
+        print(f"Scritto il file {file_path} in {sheet1}")
     except Exception as e:
         print(f"Errore durante la scrittura del file {file_path}")
 
 def cell_union(cell1, cell2):
     if are_conceptually_similar(cell1, cell2) < threshold:
-        return f"{cell1} \r\n\r\n {cell2}"
+        return f"{cell1} \r\n pippo \r\n {cell2}"
     else:
         return f"{cell1}"
 
@@ -85,36 +92,44 @@ def get_cell(row_index, col_name, sheet, file_path):
 def cut_row(row, index_row_cut=9):
     return row[:index_row_cut]
 
+def process_sheets(sheet1, sheet2):
+    already_compared = set()
+    try:
+        col1 = read_excel_column(file_path1, sheet1, col_name1)
+        col2 = read_excel_column(file_path2, sheet2, col_name2)
+        for row_index1 in range(1, row_number1 + 1):
+            for row_index2 in range(1, row_number2 + 1):
+                row1 = cut_row(read_excel_row(file_path1, sheet1, row_index=row_index1))
+                row2 = cut_row(read_excel_row(file_path1, sheet1, row_index=row_index2))
+                if row1 == row2:
+                    cell1 = get_cell(row_index1, col_name1, sheet1, file_path1)
+                    cell2 = get_cell(row_index2, col_name2, sheet2, file_path2)
+                    if (cell1, cell2) not in already_compared or (cell2, cell1) not in already_compared:
+                        cell3 = cell_union(cell1, cell2)
+                        already_compared.add((cell1, cell2))
+                        print(cell3)
+                        col3.append(cell3)
+                    write_excel_with_dataframe(file_path_output, pd.DataFrame({"TriggerCondition" :col3}))
+    except Exception as e:
+        print(f"La pagina {sheet2} nel file {file_path2} non ha una colonna chiamata {col_name2}")
 
 if __name__ == '__main__':
     sheet_names1 = list_of_sheets(file_path1)
     sheet_names2 = list_of_sheets(file_path2)
-    cell3 = ""
-    col3 = []
-    for sheet1 in sheet_names1:
-        #try:
-        row_number1 = number_of_row(file_path1, sheet1)
-        try:
-            col1 = read_excel_column(file_path1, sheet1, col_name1)
+    threads = []
+    with ThreadPoolExecutor() as executor:
+
+        for sheet1 in sheet_names1:
+            row_number1 = number_of_row(file_path1, sheet1)
             for sheet2 in sheet_names2:
-                try:
-                    col2 = read_excel_column(file_path2, sheet2, col_name2)
-                    row_number2 = number_of_row(file_path2, sheet2)
-                    for row_index1 in range(1, row_number1 + 1):
-                        for row_index2 in range(1, row_number2 + 1):
-                            row1 = cut_row(read_excel_row(file_path1, sheet1, row_index=row_index1))
-                            row2 = cut_row(read_excel_row(file_path1, sheet1, row_index=row_index2))
-                            if row1 == row2:
-                                cell1 = get_cell(row_index1, col_name1, sheet1, file_path1)
-                                cell2 = get_cell(row_index2, col_name2, sheet2, file_path2)
-                                if are_conceptually_similar(cell1, cell2):
-                                    cell3 = cell_union(cell1, cell2)
-                                else:
-                                    cell3 = cell1
-                                col3.append(cell3)
-                                write_excel_with_dataframe(file_path_output, pd.DataFrame({"TriggerCondition" :col3}))
-                except Exception as e:
-                    print(f"La pagina {sheet2} nel file {file_path2} non ha una colonna chiamata {col_name2}")
-        except Exception as e:
-            print(f"La pagina {sheet1} nel file {file_path1} non ha una colonna chiamata {col_name1}")
+                row_number2 = number_of_row(file_path2, sheet2)
+                if sheet1 == sheet2:
+                    print(f"Analizzo il foglio {sheet1}")
+                    executor.submit(process_sheets, sheet1, sheet2)
+                    #thread = threading.Thread(target=process_sheets, args=(sheet1, sheet2))
+                    #threads.append(thread)
+                    #thread.start()
+        for thread in threads:
+            thread.join()
+
 print("Ho finito")
